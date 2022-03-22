fa993b460d9446eda784998f45e3d5a9180d3754
Catalogo81 <peacematjato@gmail.com>
2022-03-22 21:13:45 +0200

    can now access home page just by entering a name

--- app/src/main/java/com/example/edu_learning/Registration/Registration.java
+++ app/src/main/java/com/example/edu_learning/Registration/Registration.java
@@ -2,15 +2,44 @@ package com.example.edu_learning.Registration;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.edu_learning.Home.Home;
 import com.example.edu_learning.R;
 
 public class Registration extends AppCompatActivity {
+
+    EditText etName;
+    Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        btnRegister = findViewById(R.id.btnRegister);
+        etName = findViewById(R.id.etName);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(etName.getText().toString().isEmpty())
+                {
+                    Toast.makeText(Registration.this, "Please enter required details!", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    startActivity(new Intent(Registration.this, Home.class));
+                }
+            }
+        });
+
     }
 }
